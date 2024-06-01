7e66928d93d953c2d1d7b3a18f6ccc6bce09a245
Massimo Palmisano <palmisano.massimo96@gmail.com>
2024-06-01 12:17:27 +0200

    added missing configuration fields

--- input.tf
+++ input.tf
@@ -23,5 +23,20 @@ variable "configuration" {
     function_name                  = string
     description                    = optional(string, "")
     handler                        = string
+    layers                         = optional(list(string), [])
+    runtime                        = optional(string, "python3.11")
+    architectures                  = optional(list(string), ["x86_64"])
+    timeout                        = optional(number, 10)
+    environment_variables          = optional(map(string), {})
+    cloudwatch_logs_days_retention = optional(number, 7)
+    tracing_mode                   = optional(string, "PassThrough")
+    ipv6_allowed_for_dual_stack    = optional(bool, false)
+    subnet_ids                     = optional(set(string), [])
+    security_group_ids             = optional(set(string), [])
+    permissions = optional(list(object({
+      effect    = optional(string, "Allow")
+      actions   = list(string)
+      resources = list(string)
+    })), [])
   })
 }
